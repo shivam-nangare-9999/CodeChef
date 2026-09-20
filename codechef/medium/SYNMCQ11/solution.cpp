836b9f32b5052dce34edf266e01be0941c1d4217
Shivam Nangare <shivamnangare63@gmail.com>
2026-09-20 10:51:54 +0530

    ✅ SYNMCQ11 | Medium | C++ | Runtime: N/A | Memory: N/A | 2026-09-20

--- codechef/medium/SYNMCQ11/solution.cpp
+++ codechef/medium/SYNMCQ11/solution.cpp
@@ -1,14 +1,16 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    int s = 14;
-    
-    // add variable area here
-    int area = 14 * 14;
-    
-    int cost = area * 7;
-    
-    cout << area << endl;
-    cout << "$" << cost;
+
+  int a = -50;
+  int b = 40;
+  int sum = a + b;
+  int product = a * b;
+  int quotient = a / b;
+  cout << sum << endl;
+  cout << product << endl;
+  cout << quotient;
+  
+  
 }
